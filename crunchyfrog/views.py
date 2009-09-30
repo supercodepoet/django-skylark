a058e8be046b26e2325dee375dd8f9927d3c27ec
robmadole@robert-madoles-macbook-pro-2.local <robmadole@robert-madoles-macbook-pro-2.local>
2009-09-30 17:10:37 -0500

    Adding mimetype sniffer to the media_by_token view

--- crunchyfrog/views.py
+++ crunchyfrog/views.py
@@ -1,9 +1,12 @@
+import mimetypes
+
 from django.http import HttpResponse, HttpResponseNotFound
 from django.core.exceptions import SuspiciousOperation
 from crunchyfrog import media_cache
 
 def media_by_token(request, token, template_name):
     try:
-        return HttpResponse(media_cache.get(token, template_name))
+        mimetype = mimetypes.guess_type(template_name)[0] or 'application/octet-stream'
+        return HttpResponse(media_cache.get(token, template_name), mimetype=mimetype)
     except SuspiciousOperation as so:
         return HttpResponseNotFound(so)
