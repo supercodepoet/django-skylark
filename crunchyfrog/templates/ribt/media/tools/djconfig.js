1e9389dfe9d325fd9aa9865a351eb3838284a918
Joe Kueser <devnull@localhost>
2010-03-16 16:27:15 -0500

    Fixing back button issue.  Pointing to cached version of iframe_history.html used by Dojo's history functionality.

--- crunchyfrog/templates/ribt/media/tools/djconfig.js
+++ crunchyfrog/templates/ribt/media/tools/djconfig.js
@@ -2,6 +2,7 @@
 if (!djConfig) {
     var djConfig = {
         parseOnLoad: false,
-        debugAtAllCosts: {{ crunchyfrog_internals.settings.CRUNCHYFROG_DOJO_DEBUGATALLCOSTS|lower }}
+        debugAtAllCosts: {{ crunchyfrog_internals.settings.CRUNCHYFROG_DOJO_DEBUGATALLCOSTS|lower }},
+        dojoIframeHistoryUrl: "{{ cache_url }}/addon/dojo/resources/iframe_history.html"
     }
 }
