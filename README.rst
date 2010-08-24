f6350803e7b6ba0c04b6ba0d6166751ab704ad7c
robmadole <robmadole@gmail.com>
2010-08-24 17:16:34 -0500

    Bit of modernizing on the package

--- README.rst
+++ README.rst
@@ -5,44 +5,34 @@ CrunchyFrog is a Django app.  You can install it with setup tools::
 
     python setup.py install
 
-Inside your settings.py add the following:
+Inside your settings.py add the following::
 
-INSTALLED_APPS = (
-    'crunchyfrog',
-)
+    INSTALLED_APPS = (
+        'crunchyfrog',
+    )
 
 Developers
 ==========
 
 Start out in the CrunchyFrog directory.
 
-First, create a virtualenv that can be used to isolate development::
-
-    virtualenv --no-site-packages env
-
-Now source the activate script::
-
-    source env/bin/activate
-
 Let's bootstrap buildout::
 
-    python bootstrap.py
+    python bootstrap.py --distribute
 
 Now run buildout::
 
-    ./bin/buildout --distribute
+    ./bin/buildout
 
 It will take a bit, downloading all the dependencies it needs.  Once it's
 finished try running the tests::
 
-    ./bin/test-11 (for testing Django 1.1)
-    ./bin/test-12 (for testing with Django 1.2)
+    ./bin/test
 
 Documentation
 =============
 
-Docs are in the docs/ directory.  It was written with Sphinx and rST
-(Re-structured text).
+Docs are in the :file:`docs` directory.  It was written using Sphinx.
 
 To build the documentation::
 
@@ -50,4 +40,4 @@ To build the documentation::
     make html
 
 The Makefile should then build the html version and stick the output in the
-docs/_build directory.
+:file:`docs/_build directory`.
--- setup.py
+++ setup.py
@@ -1,27 +1,36 @@
 from setuptools import setup, find_packages
-import sys, os
+import os
 
 from pkginfo import version
 
-setup(name='django-crunchyfrog',
-      version=version,
-      description="Web 2.0 developement, lightly killed, and covered in chocolate",
-      long_description="""\
-""",
-      classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
-      keywords='web 2 html css mvc django dojo clevercss yaml',
-      author='Rob Madole',
-      author_email='robmadole@gmail.com',
-      url='http://robmadole.com',
-      license='BSD',
-      packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
-          'pyyaml==3.09',
-          'pytidylib==0.2.1',
-      ],
-      entry_points="""
-      # -*- Entry points: -*-
-      """,
-      )
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+NEWS = open(os.path.join(here, 'NEWS.txt')).read()
+
+install_requires = [
+    # http://packages.python.org/distribute/setuptools.html#declaring-dependencies
+    'pyyaml==3.09',
+    'pytidylib==0.2.1',
+]
+
+setup(
+    name='django-crunchyfrog',
+    version=version,
+    description="",
+    long_description=README + '\n\n' + NEWS,
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='web 2 html css mvc django dojo clevercss yaml',
+    author='Rob Madole',
+    author_email='robmadole@gmail.com',
+    url='http://robmadole.com',
+    license='BSD',
+    packages=find_packages('src'),
+    package_dir = {'': 'src'},
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+      'pyyaml==3.09',
+      'pytidylib==0.2.1',
+    ],
+    entry_points={}
+)
